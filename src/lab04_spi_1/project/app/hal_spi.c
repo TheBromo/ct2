3bcfb0ebeb54088c85105918f89e5c1c97970656
manuel <manuel@strenge.ch>
2025-03-15 11:50:41 +0100

    lab4

--- src/lab04_spi_1/project/app/hal_spi.c
+++ src/lab04_spi_1/project/app/hal_spi.c
@@ -13,12 +13,14 @@
  * -- $Id: hal_spi.c 4707 2019-02-26 09:32:59Z ruan $
  * ------------------------------------------------------------------
  */
-#include <reg_stm32f4xx.h>
 #include "hal_spi.h"
+#include <reg_stm32f4xx.h>
+#include <stdint.h>
+#include <stdio.h>
 
-#define BIT_TXE  (uint32_t)0x00000002
+#define BIT_TXE (uint32_t)0x00000002
 #define BIT_RXNE (uint32_t)0x00000001
-#define BIT_BSY  (uint32_t)0x00000080
+#define BIT_BSY (uint32_t)0x00000080
 
 static void set_ss_pin_low(void);
 static void set_ss_pin_high(void);
@@ -27,57 +29,73 @@ static void wait_10_us(void);
 /*
  * according to description in header file
  */
-void hal_spi_init(void)
-{
-    RCC->APB2ENR |= 0x00001000;       // enable SPI clock
-    RCC->AHB1ENR |= 0x00000001;       // start clock on GPIO A
-    
-    
-    // configure GPIOs
-    // P5.5 --> PA4 (NSS)       --> output
-    // P5.6 --> PA5 (SCLK)      --> AF5 (see datasheet)
-    // P5.7 --> PA6 (MISO)      --> AF5 (see datasheet)
-    // P5.8 --> PA7 (MOSI)      --> AF5 (see datasheet)
-    // P5.9 --> PA8 (NSBUF)     --> input
-    
-    GPIOA->OSPEEDR &= 0xFFFF00FF;   // clear PA4-PA7
-    GPIOA->OSPEEDR |= 0x0000FF00;   // set PA4-PA7 to very high speed
-    
-    GPIOA->MODER &= 0xFFFC00FF;     // clear mode on PA4-PA8 --> PA8 = input
-        
-    GPIOA->MODER |= 0x0000A800;     // set alternate function on PA5-PA7
-    GPIOA->MODER |= 0x00000100;     // set PA4 to output
-    
-    GPIOA->AFRL &= 0x000FFFFF;      // clear alternate function
-    GPIOA->AFRL |= 0x55500000;      // Set SPI1 (AF5) alternate function PA5-PA7
-    
-    // configure SPI
-    SPI1->CR2 = 0x0000;             // set spi to default state
-    SPI1->CR1 = 0x0000;             // set spi to default state
+void hal_spi_init(void) {
+  RCC->APB2ENR |= 0x00001000; // enable SPI clock
+  RCC->AHB1ENR |= 0x00000001; // start clock on GPIO A
 
-    // add your SPI configs below (based on reference manual)
-    
-    /// STUDENTS: To be programmed
+  // configure GPIOs
+  // P5.5 --> PA4 (NSS)       --> output
+  // P5.6 --> PA5 (SCLK)      --> AF5 (see datasheet)
+  // P5.7 --> PA6 (MISO)      --> AF5 (see datasheet)
+  // P5.8 --> PA7 (MOSI)      --> AF5 (see datasheet)
+  // P5.9 --> PA8 (NSBUF)     --> input
 
+  GPIOA->OSPEEDR &= 0xFFFF00FF; // clear PA4-PA7
+  GPIOA->OSPEEDR |= 0x0000FF00; // set PA4-PA7 to very high speed
 
+  GPIOA->MODER &= 0xFFFC00FF; // clear mode on PA4-PA8 --> PA8 = input
 
+  GPIOA->MODER |= 0x0000A800; // set alternate function on PA5-PA7
+  GPIOA->MODER |= 0x00000100; // set PA4 to output
 
-    /// END: To be programmed
-    
-    set_ss_pin_high();
+  GPIOA->AFRL &= 0x000FFFFF; // clear alternate function
+  GPIOA->AFRL |= 0x55500000; // Set SPI1 (AF5) alternate function PA5-PA7
+
+  // configure SPI
+  SPI1->CR2 = 0x0000; // set spi to default state
+  SPI1->CR1 = 0x0000; // set spi to default state
+
+  // add your SPI configs below (based on reference manual)
+
+  /// STUDENTS: To be programmed
+
+  /*SPI1->CR1 |= 0x4;   // set master flag*/
+  /*SPI1->CR1 |= 0x38;   // set baud rate to 111 => 256*/
+  /*SPI1->CR1 &= 0xFBFF; // set RXONLY to 0*/
+  /*SPI1->CR1 &= 0xBFFF; // set BIDIOE to 0*/
+  /*SPI1->CR1 &= 0xCFFF; // set CRCN + CRCEN to 0*/
+  /*SPI1->CR1 |= 0x0300; // set ssm + ssi to 1*/
+                       //
+  /*SPI1->CR1 |= 0x0040; // set spe to 1 and make it scharf*/
+    SPI1->CR1 = 0xB7C;
+
+  /// END: To be programmed
+
+  set_ss_pin_high();
 }
 
 /*
  * according to description in header file
  */
-uint8_t hal_spi_read_write(uint8_t send_byte)
-{
-    /// STUDENTS: To be programmed
+uint8_t hal_spi_read_write(uint8_t send_byte) {
+  /// STUDENTS: To be programmed
+  uint8_t receive_byte = 0x0;
 
+  set_ss_pin_low();
+  wait_10_us();
+  SPI1->DR = send_byte;
+  while ((SPI1->SR & BIT_TXE) == BIT_TXE); //wait for txe flag
 
+  while ((SPI1->SR & BIT_RXNE) == BIT_RXNE); //wait for rxne flag
 
+  receive_byte = SPI1->DR;
+  while ((SPI1->SR & BIT_BSY) == BIT_BSY); //wait for bsy flag
 
-    /// END: To be programmed
+  wait_10_us();
+  set_ss_pin_high();
+
+  return receive_byte;
+  /// END: To be programmed
 }
 
 /**
@@ -87,9 +105,8 @@ uint8_t hal_spi_read_write(uint8_t send_byte)
  *
  * No returns
  */
-static void set_ss_pin_low(void)
-{
-    GPIOA->BSRR = 0x00100000;          // Set P5.5 --> PA4 low
+static void set_ss_pin_low(void) {
+  GPIOA->BSRR = 0x00100000; // Set P5.5 --> PA4 low
 }
 
 /**
@@ -99,9 +116,8 @@ static void set_ss_pin_low(void)
  *
  * No returns
  */
-static void set_ss_pin_high(void)
-{
-    GPIOA->BSRR = 0x00000010;          // Set P5.5 --> PA4 high
+static void set_ss_pin_high(void) {
+  GPIOA->BSRR = 0x00000010; // Set P5.5 --> PA4 high
 }
 
 /**
@@ -111,10 +127,9 @@ static void set_ss_pin_high(void)
  *
  * No returns
  */
-static void wait_10_us(void)
-{
-    uint8_t counter = 0;
-    while (counter < 160) {
-        counter++;
-    }
+static void wait_10_us(void) {
+  uint8_t counter = 0;
+  while (counter < 160) {
+    counter++;
+  }
 }
